b06e637db9aba4ca9d09c5dfb52594044f90e8cd
Andri Darmuli <64353589+ndridm2@users.noreply.github.com>
2023-12-07 17:23:46 +0700

    second commit

--- android/app/src/main/kotlin/com/example/chiks_restaurant_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chiks_restaurant_app/MainActivity.kt
@@ -1,2 +1,10 @@
 package com.example.chiks_restaurant_app
 
+<<<<<<< HEAD
+=======
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity(){
+
+}
+>>>>>>> 9c1f572 (second commit)
